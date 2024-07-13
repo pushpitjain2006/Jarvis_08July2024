504850be56daf656bce7f31d4527a53da098b99a
Pushpit jain <pj325284@gmail.com>
2024-07-13 14:49:54 +0530

    Update maino.py

--- maino.py
+++ maino.py
@@ -7,10 +7,12 @@ import webbrowser
 from dict import websites
 import requests
 import os
+
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
-
 pygame.mixer.init()
+
+
 def speak(text):
     tts = gTTS(text)
     tts.save('temp.mp3')
@@ -22,24 +24,29 @@ def speak(text):
     pygame.mixer.music.unload()
     os.remove("temp.mp3")
 
-his=[]
 
-def AI(c, history=[]):
+his = []
+
+
+def AI(c, history=None):
+    if history is None:
+        history = []
     model_name = "microsoft/DialoGPT-medium"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
     new_user_input_ids = tokenizer.encode(c + tokenizer.eos_token, return_tensors='pt')
     bot_input_ids = torch.cat([torch.tensor(history, dtype=torch.int64), new_user_input_ids],
                               dim=-1) if history else new_user_input_ids
-    attention_mask = torch.ones_like(bot_input_ids)  
+    attention_mask = torch.ones_like(bot_input_ids)
     if history:
-        attention_mask[:, :history[-1].shape[-1]] = 0  
+        attention_mask[:, :history[-1].shape[-1]] = 0
     chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id,
                                       attention_mask=attention_mask)
     response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-    his.append(chat_history_ids)  
+    his.append(chat_history_ids)
     print(response)
     speak(response)
+
 
 def google_search(query):
     base_url = "https://www.google.com/search?q="
@@ -62,9 +69,9 @@ def processcommand(c: str):
         c = c.replace("search", "")
         google_search(c.replace(" on google", ""))
     elif 'news' in c:
-        r = requests.get(News_api_link)
-        if r.status_code == 200:
-            data = r.json()
+        req_get = requests.get(News_api_link)
+        if req_get.status_code == 200:
+            data = req_get.json()
             articles = data.get('articles', [])
             for article in articles:
                 speak(article['title'])
@@ -72,15 +79,16 @@ def processcommand(c: str):
             speak("Sorry unable to get News currently")
     elif 'define' in c:
         word = c[7::]
-        r = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
-        if r.status_code == 200:
-            data = r.json()
+        req_get = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+        if req_get.status_code == 200:
+            data = req_get.json()
             speak(f"the word {word} is defined as" + data[0]["meanings"][0]["definitions"][0]["definition"])
         else:
-            AI(c,his)
+            AI(c, his)
 
     else:
-        AI(c,his)
+        AI(c, his)
+
 
 if __name__ == "__main__":
     print("Initializing Jarvis.....")
@@ -90,7 +98,7 @@ if __name__ == "__main__":
         try:
             with sr.Microphone() as source:
                 print("Say something!")
-                audio = r.listen(source,phrase_time_limit=1, timeout=2)
+                audio = r.listen(source, phrase_time_limit=1, timeout=2)
             command = r.recognize_google(audio)
             print(command)
             if command.lower() == "jarvis":
